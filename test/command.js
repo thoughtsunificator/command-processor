ba9135ed097ee538f16223d285b3c2eeb8cc035f
thoughtsunificator <oss@thoughtsunificator.me>
2021-11-14 00:23:21 +0000

    tests

--- test/command.js
+++ test/command.js
@@ -1,9 +1,19 @@
+import assert from "assert"
+
 import { Command } from "../index.js"
 
-describe("processor", () => {
+describe("Command", () => {
 
 	it("instance", () => {
-
+		const command = new Command("foo", "bar")
+		assert.deepEqual(command.syntax, "foo")
+		assert.deepEqual(command.data, "bar")
+		assert.throws(() => {
+			command.syntax = []
+		})
+		assert.throws(() => {
+			command.data = []
+		})
 	})
 
 })
--- test/processor.js
+++ test/processor.js
@@ -1,13 +1,37 @@
-import { Processor } from "../index.js"
+import assert from "assert"
 
-describe("processor", () => {
+import { Processor, Command } from "../index.js"
+
+describe("Processor", () => {
 
 	it("instance", () => {
-
+		const processor = new Processor([1])
+		assert.deepEqual(processor.commands, [1])
+		assert.strictEqual(Processor.PREFIX_COMMAND, "/")
+		assert.strictEqual(Processor.ERROR.COMMAND_NOT_FOUND, "COMMAND_NOT_FOUND")
+		assert.strictEqual(Processor.ERROR.MISSING_ARGS, "MISSING_ARGS")
+		assert.doesNotThrow(() => {
+			processor.commands = []
+		})
 	})
 
 	it("run", () => {
-
+		const processor = new Processor([
+			new Command("foo <arg>", "somedata"),
+			new Command("bar <arg1>", "somedata2"),
+			new Command("test <arg1>", "somedata3"),
+			new Command("test2 <arg1> <arg2>", "somedata4"),
+		])
+		assert.deepEqual(processor.run("foo hello"), { error: Processor.ERROR.COMMAND_NOT_FOUND })
+		assert.deepEqual(processor.run("/foo"), { error: Processor.ERROR.MISSING_ARGS })
+		assert.deepEqual(processor.run("/bar"), { error: Processor.ERROR.MISSING_ARGS })
+		assert.deepEqual(processor.run("/foo hello"), { data: "somedata", args: "hello" })
+		assert.deepEqual(processor.run("/bar test"), { data: "somedata2", args: "test" })
+		assert.deepEqual(processor.run("/test test dsa dsadsa dsadsa"), { data: "somedata3", args: "test dsa dsadsa dsadsa" })
+		assert.deepEqual(processor.run("/test2 test dsa dsadsa dsadsa"), { data: "somedata4", args: {
+			arg1: "test",
+			arg2: "dsa dsadsa dsadsa"
+		} })
 	})
 
 })
